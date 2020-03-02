d525601a92967c9478e40455ad0401cde6b4b822
Christian Martínez <christt105@gmail.com>
2020-03-02 19:00:35 +0100

    Blur filter

--- AR/CV/linearFilters.py
+++ AR/CV/linearFilters.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('hk.png', 1)
+img = cv2.imread('noise.png', 1)
 img = img/255
 
 kernel = np.ones((5, 5))/25
@@ -9,14 +9,20 @@ kernel = np.ones((5, 5))/25
 rows, col, a = img.shape
 
 imgf = np.zeros((rows, col))
+summ = np.zeros(9)
 
-for i in range(0, col):
-    for j in range(0, rows):
-        summ = np.zeros(3)
-        for k in range(i - 5, i+5):
-            for l in range(j - 5, j+5):
-                summ += img[j, i]/25
-        imgf[j, i] = summ.mean()
+for i in range(1, rows - 1):
+    for j in range(1, col - 1):
+        summ[0] = img[i-1, j-1, 0]
+        summ[1] = img[i, j-1,0]
+        summ[2] = img[i+1, j-1,0]
+        summ[3] = img[i-1, j,0]
+        summ[4] = img[i, j,0]
+        summ[5] = img[i+1, j,0]
+        summ[6] = img[i-1, j+1,0]
+        summ[7] = img[i, j+1,0]
+        summ[8] = img[i+1, j+1,0]
+        imgf[i, j] = summ.mean()
 
 cv2.imshow('Hollow Knight', img)
 cv2.imshow('Hollow filtered', imgf)
