e233951c6fc5b12eae12597a3b1306ddafd3287d
Christian Martínez <christt105@gmail.com>
2020-04-22 12:47:27 +0200

    Have only one copy of vertices of every object

--- AR/CG/Object.js
+++ AR/CG/Object.js
@@ -1,6 +1,6 @@
 class Object {
-    constructor(name) {
-        this.name = name;
+    constructor(type) {
+        this.type = type;
 
         this.x = 0.0;
         this.y = 0.0;
@@ -10,27 +10,6 @@ class Object {
 
         this.mirror_h = false;
         this.mirror_v = false;
-    }
-
-    createBuffers() {
-        this.createVertexBuffer(this.vertices, this.colors);
-        this.createIndexBuffer(this.indices);
-    }
-
-    createVertexBuffer(vertices, colors) {
-        this.id_v = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.id_v);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-        this.id_c = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.id_c);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-    }
-
-    createIndexBuffer(indices) {
-        this.id_i = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.id_i);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
     }
 
     setPosition(x, y) {
@@ -52,13 +31,7 @@ class Object {
     }
 
     draw() {
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.id_v);
-        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.id_c);
-        gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 3, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.id_i);
+        this.type.bindBuffers();
 
         var pMatrix = mat4.create();
         mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
@@ -79,104 +52,6 @@ class Object {
         gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
         gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 
-        gl.drawElements(gl.TRIANGLES, this.size_i * 3, gl.UNSIGNED_SHORT, 0);
-    }
-}
-
-class Triangle extends Object {
-    constructor(width, height) {
-        super("Triangle");
-
-        this.width = width;
-        this.height = height;
-
-        this.vertices = [
-            0.0, height, 0.0,
-            -width * 0.5, -width * 0.5, 0.0,
-            width * 0.5, -width * 0.5, 0.0
-        ]
-
-        this.colors = [
-            1.0, 0.0, 0.0,
-            0.0, 1.0, 0.0,
-            0.0, 0.0, 1.0
-        ]
-
-        this.indices = [
-            0, 1, 2
-        ]
-
-        this.size_v = 3;
-        this.size_c = 3;
-        this.size_i = 1;
-
-        this.createBuffers();
-    }
-}
-
-class Quad extends Object {
-    constructor(width) {
-        super("Quad");
-
-        this.width = width;
-
-        this.vertices = [
-            -width * 0.5, -width * 0.5, 0.0,
-            width * 0.5, -width * 0.5, 0.0,
-            width * 0.5, width * 0.5, 0.0,
-            -width * 0.5, width * 0.5, 0.0
-        ]
-
-        this.colors = [
-            1.0, 0.0, 0.0,
-            0.0, 0.0, 0.0,
-            0.0, 0.0, 1.0,
-            1.0, 1.0, 1.0
-        ]
-
-        this.indices = [
-            0, 1, 2,
-            2, 3, 0
-        ]
-
-        this.size_v = 4;
-        this.size_c = 4;
-        this.size_i = 2;
-
-        this.createBuffers();
-    }
-}
-
-class Cube extends Object {
-    constructor(width) {
-        super("Cube");
-
-        this.width = width;
-
-        this.vertices = [
-            -width * 0.5, -width * 0.5, -width * 0.5,
-            width * 0.5, -width * 0.5, -width * 0.5,
-            width * 0.5, width * 0.5, -width * 0.5,
-            -width * 0.5, width * 0.5, -width * 0.5,
-
-            -width * 0.5, -width * 0.5, width * 0.5,
-            width * 0.5, -width * 0.5, width * 0.5,
-            width * 0.5, width * 0.5, width * 0.5,
-            -width * 0.5, width * 0.5, width * 0.5,
-        ]
-
-        this.indices = [
-            0, 1, 3, 3, 1, 2,
-            1, 5, 2, 2, 5, 6,
-            5, 4, 6, 6, 4, 7,
-            4, 0, 7, 7, 0, 3,
-            3, 2, 7, 7, 2, 6,
-            4, 5, 0, 0, 5, 1
-        ]
-
-        this.size_v = 8;
-        this.size_i = 12;
-
-        this.createBuffers();
+        this.type.draw();
     }
 }
